535faa489c9c7f9b9c7db3a1f3d3e132a20d7a8a
Tal Frumkin <tal.frumkin@gmail.com>
2025-02-07 04:50:06 +0200

    feat: dockerization

--- backend/src/config/config.ts
+++ backend/src/config/config.ts
@@ -1,7 +1,7 @@
-import { Config } from "../../common"
+import { Config } from "../common"
 import * as dotenv from 'dotenv';
 
-dotenv.config({ path: __dirname + '/../.env' });
+dotenv.config({ path: __dirname + '../../../.env' });
 
 const Settings: Config  = { 
     port: process.env.PORT ? parseInt(process.env.PORT) : 4000
